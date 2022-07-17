0af1737b18dc9802ab57e66a1edd502af4b72da9
YenLap <s810643@gmail.com>
2022-07-17 11:09:59 +0800

    Add template and static

--- app/views.py
+++ app/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello world!")
+    # return HttpResponse("Hello world!")
+    return render(request, "index.html")
